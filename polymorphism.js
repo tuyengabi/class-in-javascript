669e0e9c38e9208504cdd50b4f4bfc60093ab002
tuyengabi <tuyen.gabi@gmail.com>
2019-05-30 22:50:47 +0200

    modify polymorphism.js

--- polymorphism.js
+++ polymorphism.js
@@ -8,3 +8,13 @@ class Animal {
     console.log('Generic Animal Sound!!');
   }
 }
+
+class Dog extends Animal {
+  constructor(_name) {
+    super(_name);
+  }
+
+  makeSound() {
+    console.log("Woof! Woof!");
+  }
+}
