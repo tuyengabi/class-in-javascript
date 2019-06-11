1aa45e7e5b2f4605010ece536d58d5b8d2773730
tuyengabi <tuyen.gabi@gmail.com>
2019-06-11 09:23:17 +0200

    modify ListBinding.js , index.html

--- ListBinding.js
+++ ListBinding.js
@@ -1,16 +1,17 @@
 
 class ListBinding {
   constructor(element) {
-    this.listElement = element;
+    this.listElement = "element";
     this.textList = [];
   }
 
+
   update() {
     //remove all existing in <li> tag
-    while(this.listElement.firstChild) {
+    while (this.listElement.firstChild) {
       this.listElement.removeChild(this.listElement.firstChild);
     }
 
-    
   }
+
 }
